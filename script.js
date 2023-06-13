bfadc3e39d34a1906a5714545ee513a7eeb8ea4d
therosenstock <fabiolars2009@hotmail.com>
2023-06-13 18:07:16 -0300

    menu lateral e input range

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-//globals
+//PADROES
+const default_color = "red";
+const default_size = 16;
+const default_mode = "COLOR"
+
+//GLOBAIS
 const container = document.querySelector("#container");
 let grids = [];
 let draw = false;
+let current_size = default_size;
+let current_color = default_color;
+let current_mode = default_mode;
+let gridSize = 600 / current_size;
+
+//DOM HTML
+let itens = document.querySelector(".settings");
+let range = document.querySelector("#tamanho");
+let output = document.querySelector("#resultado");
+
+//EVENTOS
+range.onmousemove = (e) => updateSizeValue(e.target.value)
+range.onchange = (e) => changeSize(e.target.value)
 
 
 function setSize(){
-    let size = 16*16;
+    let qtde = current_size*current_size;
     
-    for(let i=0; i<size; i++){
+    for(let i=0; i<qtde; i++){
         grids.push({
             id: i
         })
@@ -17,9 +35,17 @@ function setSize(){
 
 function rendergrid(){
     const gridarray = grids.map(grid => `
-    <div class="grid" data-id="${grid.id}"> </div>`);
+    <div class="grid" data-id="${grid.id}"> </div>`);    
 
     container.innerHTML = gridarray.join(``);
+
+    const grid = document.querySelectorAll('.grid');
+    Array.from(grid).forEach((square) => {
+        square.style.height = `${gridSize}px`;
+        square.style.width = `${gridSize}px`
+    })
+    console.log(grid)
+    
 }
 
 function events(){ 
@@ -30,12 +56,12 @@ function events(){
 
         square.addEventListener('click', () => {
             
-            square.style.backgroundColor = "#333";
+            square.style.backgroundColor = current_color;
 
         })
         square.addEventListener('mouseover', () => {
             if(draw){
-                square.style.backgroundColor = "#333";
+                square.style.backgroundColor = current_color;
             }
         })
         
@@ -49,6 +75,33 @@ function stopDraw(){
     draw = false 
 }
 
+function changeSize(value){
+    current_size = value;
+    console.log(value)
+    gridSize = 600 / current_size;
+    // updateSizeValue(value);
+    // clear();
+    // setSize();
+    // rendergrid();
+    // events();
+}
+
+function updateSizeValue(value){
+    output.innerHTML = `${value} X ${value}`
+} 
+
+function clickMenu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+   
+}
+
+function clear(){
+    container.innerHTML = ''
+}
 
 setSize();
 rendergrid();
