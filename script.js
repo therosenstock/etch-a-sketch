53f41f9090016e74d43c4a344fbc60a6684c8540
therosenstock <fabiolars2009@hotmail.com>
2023-06-14 20:27:55 -0300

    style ajust

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //PADROES
-const default_color = "red";
-const default_size = 28;
-const default_mode = "COLOR"
+const default_color = "rgb(33,203,33)";
+const default_size = 16;
+const default_mode = "DRAW"
 
 //GLOBAIS
 const container = document.querySelector("#container");
@@ -15,10 +15,17 @@ let gridSize = 600 / current_size;
 let itens = document.querySelector(".settings");
 let range = document.querySelector("#tamanho");
 let output = document.querySelector("#resultado");
+let cleanbtn = document.querySelector("#btn-clean");
+let eraserbtn = document.querySelector("#btn-eraser");
+let drawbtn = document.querySelector("#btn-draw");
+let colorpick = document.querySelector("#color-pick")
+
 
 //EVENTOS
 range.onmousemove = (e) => updateSizeValue(e.target.value)
 range.onchange = (e) => changeSize(e.target.value)
+colorpick.onchange = (e) => changeColor(e.target.value)
+
 
 
 function setSizeGrid(){
@@ -51,24 +58,38 @@ function renderGrid(grids){
 
 function eventGrid(){ 
     const grid = document.querySelectorAll('.grid');
+   
     Array.from(grid).forEach((square) => {
 
         square.getAttribute('[data-id]');
-
+        
         square.addEventListener('click', () => {
-            
-            square.style.backgroundColor = current_color;
+            if(current_mode == "ERASER"){
+                square.style.backgroundColor = "#fff";
+
+            }else{
+                square.style.backgroundColor = current_color;
+            }
 
         })
         square.addEventListener('mouseover', () => {
             if(draw){
-                square.style.backgroundColor = current_color;
-
+                if(current_mode == "ERASER"){
+                    square.style.backgroundColor = "#fff";
+    
+                }else{
+                    square.style.backgroundColor = current_color;
+                }
             }
         })
         square.addEventListener('mouseleave', () => {
             if(draw){
-                square.style.backgroundColor = current_color;
+                if(current_mode == "ERASER"){
+                    square.style.backgroundColor = "#fff";
+    
+                }else{
+                    square.style.backgroundColor = current_color;
+                }
             }
         })
         
@@ -88,7 +109,6 @@ function changeSize(value){
     gridSize = 600 / current_size;
     updateSizeValue(value);
     clearGrid();
-    setSizeGrid();
 }
 
 function updateSizeValue(value){
@@ -106,6 +126,16 @@ function clickMenu(){
 
 function clearGrid(){
     container.innerHTML = '';
+    setSizeGrid();
+}
+
+function changeMode (value){
+    current_mode = value;
+}
+
+function changeColor (value){
+    current_color = value;
+    current_mode = "DRAW";
 }
 
 setSizeGrid();
